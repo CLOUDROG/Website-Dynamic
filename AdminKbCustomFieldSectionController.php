6f8e56dfbff091e1e00f860860aa721563eaa044
CLOUDROG <68770234+CLOUDROG@users.noreply.github.com>
2020-09-30 20:47:06 +0530

    Update AdminKbCustomFieldSectionController.php

--- AdminKbCustomFieldSectionController.php
+++ AdminKbCustomFieldSectionController.php
@@ -2,7 +2,7 @@
 /**
  * DISCLAIMER
  *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to neweror Do at Ur Own Risk.
  * versions in the future.If you wish to customize PrestaShop for your
  * needs please refer to http://www.prestashop.com for more information.
  * We offer the best and most useful modules PrestaShop and modifications for your online store.
